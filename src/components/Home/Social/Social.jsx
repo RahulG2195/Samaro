ea0aa4c125a70fd3b6b8076953a35acb707a8ccc
Siddhesh-Dhamale <webdeveloper.crezvatic@gmail.com>
2024-08-06 10:19:24 +0530

    dumy insta images shown

--- src/components/Home/Social/Social.jsx
+++ src/components/Home/Social/Social.jsx
@@ -1,29 +1,79 @@
-import React from 'react';
+// import React from 'react';
+// import "./Social.css";
+// import { InstagramEmbed } from 'react-social-media-embed';
+
+// const Social = () => {
+//     return (
+//         <section className="social">
+//             <div className="container h-screen">
+//                 <h2 className="text-center">let's get <span>social</span></h2>
+//                 <div className='d-flex align-items-center justify-content-center mt-4 mb-3 instagramimg'>
+//                     <div className='instaLogo'>
+//                         <img src="/assets/images/home/social/Group 29049.svg" alt="Instagram Logo" className='w-75' />
+//                     </div>
+//                     <div>
+//                         <a href="https://www.instagram.com/samaroflooring?igsh=amowMmMyeHU1eXVh" target="_blank" rel="noopener noreferrer" className='idAndbio'>
+//                             <span className='instaid'>@SAMAROFLOORING</span>
+//                             <p className='instabio'>Where Indian Craftsmanship Meets Global Luxury,<br />Ready to Elevate Your World.</p>
+
+//                         </a>
+//                     </div>
+//                 </div>
+
+//                 <iframe className='instagramSliider' src="https://widget.tagembed.com/156029?view" style={{ width: "100%", height: "300px" }} allowtransparency="true"></iframe>
+//             </div>
+//         </section>
+//     )
+// }
+
+// export default Social;
+
+
+import React from 'react'
 import "./Social.css";
-import { InstagramEmbed } from 'react-social-media-embed';
 
 const Social = () => {
     return (
         <section className="social">
-            <div className="container h-screen">
-                <h2 className="text-center">let's get <span>social</span></h2>
+            <div className="container">
+                <h2 className="text-center ">let's get <span>social</span></h2>
                 <div className='d-flex align-items-center justify-content-center mt-4 mb-3 instagramimg'>
-                    <div className='instaLogo'>
-                        <img src="/assets/images/home/social/Group 29049.svg" alt="Instagram Logo" className='w-75' />
-                    </div>
+                    <div className='instaLogo'><img src="/assets/images/home/social/Group 29049.svg" alt="err" className='w-75' /></div>
                     <div>
-                        <a href="https://www.instagram.com/samaroflooring?igsh=amowMmMyeHU1eXVh" target="_blank" rel="noopener noreferrer" className='idAndbio'>
-                            <span className='instaid'>@SAMAROFLOORING</span>
-                            <p className='instabio'>Where Indian Craftsmanship Meets Global Luxury,<br />Ready to Elevate Your World.</p>
-
-                        </a>
+                    <a href="https://www.instagram.com/samaroflooring?igsh=amowMmMyeHU1eXVh" target="_blank" className='idAndbio'>
+                        <span className='instaid'>@SAMAROFLOORING</span>
+                        <p className='instabio'>Where Indian Craftsmanship Meets Global Luxury, <br />Ready to Elevate Your World.</p>
+                    </a>
                     </div>
+                    {/* <a href="https://www.instagram.com/samaroflooring?igsh=amowMmMyeHU1eXVh" target="_blank"><img src="/assets/images/home/social/Group 28501.svg" alt="err" className='w-25 instagram my-2++*' /></a> */}
                 </div>
-
-                <iframe className='instagramSliider' src="https://widget.tagembed.com/156029?view" style={{ width: "100%", height: "300px" }} allowtransparency="true"></iframe>
+                {/* <div className="row justify-content-center"> */}
+                    {/* <div className="col-lg-12 col-md-12 col-sm-12 col-12"> */}
+                    <a href="https://www.instagram.com/samaroflooring?igsh=amowMmMyeHU1eXVh" target="_blank">
+                        <div className=" row justify-content-center gap-2">
+                            <div className="image col-lg-2 col-md-4 col-sm-4 col-10">
+                                <img src="/assets/images/social-media/01.webp" alt="" />
+                            </div>
+                            <div className="image col-lg-2 col-md-4 col-sm-4 col-10">
+                                <img src="/assets/images/social-media/02.webp" alt="" />
+                            </div>
+                            <div className="image col-lg-2 col-md-4 col-sm-4 col-10">
+                                <img src="/assets/images/social-media/03.webp" alt="" />
+                            </div>
+                            <div className="image col-lg-2 col-md-4 col-sm-4 col-10">
+                                <img src="/assets/images/social-media/04.webp" alt="" />
+                            </div>
+                           
+                        </div>
+                        </a>
+                    {/* </div> */}
+                {/* </div> */}
             </div>
         </section>
     )
 }
 
-export default Social;
+export default Social
+
+
+
--- src/components/Product/ProductCard.jsx
+++ src/components/Product/ProductCard.jsx
@@ -1,27 +1,49 @@
-import Link from 'next/link'
-import React from 'react'
-import './Filters.css'
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+import './Filters.css';
 
-const ProductCard = ({ frontImage, onHoverImage, title, cat_name,variation, prod_code, seo }) => {
+const ProductCard = ({ frontImage, onHoverImage, title, cat_name, variation, prod_code, seo }) => {
     return (
         <>
-            <div className="card productCard my-3 mx-0" style={{ width: '100%' }} >
+            <div className="card productCard my-3 mx-0" style={{ width: '100%' }}>
                 <Link href={`/productdetail/${seo}`}>
-                    <div className='frontimage'><img src={`/assets/images/products/AllData/${frontImage}` }className="card-img-top " alt="..." /></div>
+                    <div className='frontimage'>
+                        <Image 
+                            src={`/assets/images/products/AllData/${frontImage}`} 
+                            alt={`${title} - front view`} 
+                            layout="responsive" 
+                            width={500} 
+                            height={500} 
+                            objectFit="contain" 
+                            className="card-img-top"
+                        />
+                    </div>
                     {
-                        onHoverImage && <div className='onHoverimage'><img src={`/assets/images/products/AllData/${onHoverImage}`} className="card-img-top " alt="..." /></div>
+                        onHoverImage && 
+                        <div className='onHoverimage'>
+                            <Image 
+                                src={`/assets/images/products/AllData/${onHoverImage}`} 
+                                alt={`${title} - hover view`} 
+                                layout="responsive" 
+                                width={500} 
+                                height={500} 
+                                objectFit="contain" 
+                                className="card-img-top"
+                            />
+                        </div>
                     }
                 </Link>
 
                 <Link href={`/productdetail/${seo}`}>
                     <div className="card-body p-0 pb-1 text-center">
-                    <span className="fw-semibold  prdctName ">{title}</span>
-                    <p className='small darkBlue'>{cat_name} | {variation} | {prod_code}</p>
-                </div>
+                        <span className="fw-semibold prdctName">{title}</span>
+                        <p className='small darkBlue'>{cat_name} | {variation} | {prod_code}</p>
+                    </div>
                 </Link>
             </div>
         </>
-    )
+    );
 }
 
-export default ProductCard
+export default ProductCard;
