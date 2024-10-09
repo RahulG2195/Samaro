b3f9671408dbbc90879e34b29333c0870de3c1fc
Siddhesh-Dhamale <webdeveloper.crezvatic@gmail.com>
2024-10-09 17:38:15 +0530

    brochure  section  added in detail page

--- src/components/ProductDetail/DownloadBroch.jsx
+++ src/components/ProductDetail/DownloadBroch.jsx
@@ -1,105 +1,38 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import axios from "axios";
+import DownloadSwiper from "@/components/Downloadcenter/DownloadSwiper";
+const DownloadBroch = ({ useRed }) => {
+  const [brocardData, setBrochureData] = useState([]);
+  const [activeCategory, setActiveCategory] = useState('All');
 
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/admin/main_dcPage");
+        const data = response.data;
 
-// Initialize Swiper modules
+        const brochures = data.filter(
+          (item) => item.dc_category == "Brochure"
+        );
+        setBrochureData(brochures);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-const DownloadBroch = () => {
-  const brochureImages = [
-    {
-      src: '/assets/images/brochure/Brochure.png',
-      alt: 'Brochure 1',
-      link: '/#',
-    },
-    {
-      src: '/assets/images/brochure/Brochure.png',
-      alt: 'Brochure 2',
-      link: '/#',
-    },
-    // {
-    //   src: '/assets/images/brochure/samaro_spc_flooring_lookbook_front-1.png',
-    //   alt: 'Brochure 3',
-    //   link: '/#',
-    // },
-    // {
-    //   src: '/assets/images/brochure/samaro_spc_flooring_lookbook_front-1.png',
-    //   alt: 'Brochure 4',
-    //   link: '/#',
-    // },
-    // {
-    //   src: '/assets/images/brochure/samaro_spc_flooring_lookbook_front-1.png',
-    //   alt: 'Brochure 5',
-    //   link: '/#',
-    // },
-    // Add more brochure objects as needed
-  ];
+    fetchData();
+  }, []);
 
   return (
-    <section className='downbroch_Sec common_sec'>
-      <div className='container'>
-        <div className='sec_heading_Left my-2 my-md-4'>
-          <h1 className='section_heading'>Download <strong>Brochure</strong></h1>
-        </div>
-        <Swiper
-              modules={[ Pagination, Scrollbar, A11y]}
-          slidesPerView={2}
-          spaceBetween={10}
-          breakpoints={{
-            576: {
-              slidesPerView: 2,
-              spaceBetween: 10,
-            },
-            768: {
-              slidesPerView: 3,
-              spaceBetween: 10,
-            },
-            992: {
-              slidesPerView: 5,
-              spaceBetween: 10,
-            },
-            1200: {
-              slidesPerView: 5,
-              spaceBetween: 20,
-            },
-          }}
-          // navigation={true}
-          pagination={{ clickable: true }}
-          className='swiper-container' // Add the swiper-container className
-        >
-          {brochureImages.map((image, index) => (
-            <SwiperSlide key={index} className='row'>
-              <div className='col-md-12 col-12 px-3'>
-                <div className='brochure_imgdiv'>
-
-                  <Link href={image.link}>
-                    <Image
-                      src={image.src}
-                      alt={image.alt}
-                      width={100}
-                      height={100}
-                      layout='responsive'
-                      objectFit='cover'
-                    />
-                  </Link>
-                </div>
-              </div>
-            </SwiperSlide>
-          ))}
-          
-        </Swiper>
-      </div>
-    </section>
+    <>
+      <DownloadSwiper
+        title="Brochure"
+        cardData={brocardData}
+        active={activeCategory === "All" || activeCategory === "Brochure"}
+      />
+    </>
   );
 };
 
